8d39b0b04433a3008cf26aae8782f31a4ec39033
youtheyeon <yuseoyeon@ewhain.net>
2024-12-15 21:09:13 +0900

    feat: 메인 페이지 네비게이트 설정

--- src/pages/MainPage/components/scroll/Scroll.tsx
+++ src/pages/MainPage/components/scroll/Scroll.tsx
@@ -1,4 +1,5 @@
-import * as S from './Scorll.style';
+import { Link } from 'react-router-dom';
+import * as S from './Scroll.style';
 import Carousel from './Carousel';
 import Arrow from '../Arrow';
 import { pathToCategory } from 'constants/category';
@@ -20,7 +21,9 @@ const Scroll = ({ category }: ScrollProps) => {
         {category.title[0]} <br />
         {category.title[1]}
       </S.Title>
-      <S.ListLink color={categoryColor}>-&gt; 리스트로 보기</S.ListLink>
+      <S.ListLink color={categoryColor}>
+        <Link to={`/list/${category.category}`}>-&gt; 리스트로 보기 </Link>
+      </S.ListLink>
       <Carousel data={category.data} />
       <Arrow />
     </S.Wrapper>
