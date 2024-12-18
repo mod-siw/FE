e39370b3584a035f0c3a40c855f0e7dce24711d5
JiwonChoi0805 <won0805@ewhain.net>
2024-12-18 22:35:49 +0900

    fix: 상세페이지 union 심볼 크기 줄어듬 수정

--- src/assets/DetailUnion.tsx
+++ src/assets/DetailUnion.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgDetailUnion = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 535 535" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 402 535" {...props}>
     <path
       fill="#fff"
       fillOpacity={0.1}
       fillRule="evenodd"
-      d="M320.997 0h-107v138.331l-97.81-97.81-75.66 75.66 97.821 97.821H0v107h138.337l-97.819 97.819 75.661 75.66 97.818-97.818V535h107V396.652l97.832 97.831 75.66-75.66-97.82-97.821H535v-107H396.658l97.823-97.823-75.661-75.66-97.823 97.823z"
+      d="M254.997 0h-107v138.331l-97.81-97.81-75.66 75.66 97.82 97.821H-66v107H72.337l-97.819 97.819 75.66 75.66 97.819-97.818V535h107V396.652l97.832 97.831 75.66-75.66-97.82-97.821H469v-107H330.658l97.823-97.823-75.661-75.66-97.823 97.823z"
       clipRule="evenodd"
     />
   </svg>
